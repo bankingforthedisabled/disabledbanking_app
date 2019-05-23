fe793e01e971726cc9318a112b34e0da0e3244ed
Dylan Fernandes <dylan.fernandes00@gmail.com>
2019-05-23 12:49:06 -0400

    final setup commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './pages/app'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './config/main.scss'
+
+ReactDOM.render(<App />, document.getElementById('root'))
